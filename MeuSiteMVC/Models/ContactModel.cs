a5e8c78da60dc154fe223b8d6e7102e2cafa6e0d
Paulo Souza <paulosouzx14@gmail.com>
2024-09-05 15:06:35 +0100

    Usando DataAnnotations para requerir dados no form

---
+++ MeuSiteMVC/Models/ContactModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuSiteMVC.Models
+{
+    public class ContactModel
+    {
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "Enter Contact's Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Enter Contact's Email")]
+        [EmailAddress(ErrorMessage = "Email invalid")]
+        public string Email
+        {
+            get => _email;
+            set => _email = value.ToLower();
+        }
+
+        // Backing field for the Email property
+        private string _email;
+
+        [Required(ErrorMessage = "Enter Contact's Phone")]
+        [Phone(ErrorMessage = "Phone invalid")]
+        public string Phone { get; set; }
+
+    }
+}
