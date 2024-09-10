036ade7b4f4d47427668bcfa810d7e0845ae1828
Paulo Souza <paulosouzx14@gmail.com>
2024-09-10 13:58:13 +0100

    Criacao dos filters para que a verificacao seja feita de maneira correta

---
+++ MeuSiteMVC/Filters/RetrictPageAdm.cs
@@ -0,0 +1,37 @@
+﻿using MeuSiteMVC.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Routing;
+using Newtonsoft.Json;
+
+namespace MeuSiteMVC.Filters
+{
+    public class RetrictPageAdm : ActionFilterAttribute
+    {
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            string sessionUser = context.HttpContext.Session.GetString("sessionLogedUser");
+
+            if (string.IsNullOrEmpty(sessionUser))
+            {
+                context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+            }
+            else
+            {
+                UserModel user = JsonConvert.DeserializeObject<UserModel>(sessionUser);
+
+                // Check if user is null and only redirect then
+                if (user == null)
+                {
+                    context.Result = new RedirectToRouteResult(new RouteValueDictionary { { "controller", "Login" }, { "action", "Index" } });
+                }
+            }
+
+
+            base.OnActionExecuted(context);
+        }
+    }
+}
