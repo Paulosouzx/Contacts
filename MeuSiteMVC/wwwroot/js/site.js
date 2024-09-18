4e54ef0c1a2dc794da2457f0a65fc6064b864743
Paulo Souza <paulosouzx14@gmail.com>
2024-09-18 13:29:42 +0100

    Usando de jquery para o funcionamento do modal

--- MeuSiteMVC/wwwroot/js/site.js
+++ MeuSiteMVC/wwwroot/js/site.js
@@ -1,9 +1,23 @@
 ﻿
 
-$('.close-alert').click(function () {
+$('.close-alert').on('click', function () {
     $('.alert').hide('hide');
 });
 
 
+$(document).ready(() => {
+    getDatatable('#table-contacts');
+    getDatatable('#table-user');
+
+    // Use 'on' para eventos dinâmicos
+    $(document).on('click', '.btn-total-contacts', function () {
+        $('#modalContactsUser').modal('show');  // Abra o modal ao clicar no botão
+    });
+});
+
+
+
 let table = new DataTable('#table-contacts', '#table-user');
+
+
 
